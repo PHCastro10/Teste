593273d3284e6483ddf67581c4473ecb94421555
Pedro Henrique Carneiro de Castro <156611541+PHCastro10@users.noreply.github.com>
2025-05-02 11:41:21 -0300

    Delete 6-Modulo.py

--- 6-Modulo.py
+++
@@ -1,8 +0,0 @@
-#Em python você pode importar bibliotecas para utizar certas funcionalidades
-#Algumas bibliotecas já vem padrão com o python porem tem como importar outras
-import random
-
-print ("Ola, Mundo: 🤬")
-sorteado = random.randint(0,1000) #sera sorteado um numero inteiro
-print("O numemro sorteado foi:", sorteado)
-
